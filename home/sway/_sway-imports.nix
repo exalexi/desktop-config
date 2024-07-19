e29a43163ea6c14320681573935158e0e9e8187e
exalexi <exalexi@proton.me>
2024-07-20 00:15:37 +0200

    code layout

--- home/sway/_sway-imports.nix
+++ home/sway/_sway-imports.nix
@@ -1,11 +1,11 @@
 { pkgs, ...}: {
-    imports =
-    [ # Include the results of the hardware scan.
-      ./sway-config.nix
-      ./swayidle.nix
-      ./swaylock.nix
-      ./sway-extraconfig.nix
-      ./waybar/waybar.nix
-      ./sway-notifications.nix
-    ];
+  imports =
+  [ 
+    ./waybar/waybar.nix
+    ./sway-config.nix
+    ./sway-extraconfig.nix
+    ./sway-notifications.nix
+    ./swayidle.nix
+    ./swaylock.nix
+  ];
 }
