a47addf4079ef7dfca75280bd6d1cfd2f54992cb
exalexi <exalexi@proton.me>
2025-03-16 19:50:33 +0100

    Greetd removed initial session

--- config/system/greetd.nix
+++ config/system/greetd.nix
@@ -6,10 +6,6 @@
     restart = false;
     settings = {
       default_session.command = "tuigreet --cmd 'dbus-run-session sway' -t -r --asterisks -g 'Hiya'";
-      initial_session = {
-        user = "lexi";
-        command = "dbus-run-session sway";
-      };
     };
   };
 }
